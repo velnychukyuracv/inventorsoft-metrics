68284d4d0e4e39ef61aa10ba1784f2c5900ff042
Anastasia Mardar <anastasia.mardar@gmail.com>
2018-06-07 00:39:28 +0300

    Create Groups component

--- ui/src/app/sidebar/sidebar.module.ts
+++ ui/src/app/sidebar/sidebar.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GroupsComponent } from './groups/groups.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [GroupsComponent]
 })
 export class SidebarModule { }
