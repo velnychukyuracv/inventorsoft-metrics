d3b981838d4b96fa84121546aae3c0941ad30f70
Anastasia Mardar <anastasia.mardar@gmail.com>
2018-06-07 17:15:41 +0300

    Create Menu component

--- ui/src/app/sidebar/groups/groups.component.ts
+++ ui/src/app/sidebar/groups/groups.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { Group, GroupsService } from '../../common/services/groups.service';
+import { GroupsService } from '../../common/services/groups.service';
+import { Group } from '../../common/models/group.model';
+import { first } from 'rxjs/internal/operators/first';
 
 @Component({
-  selector: 'app-groups',
-  templateUrl: './groups.component.html',
-  styleUrls: ['./groups.component.scss']
+    selector   : 'app-groups',
+    templateUrl: './groups.component.html',
+    styleUrls  : ['./groups.component.scss']
 })
 export class GroupsComponent implements OnInit {
 
     groups: Group[]
 
-  constructor(private groupsService: GroupsService) { }
+    constructor(private groupsService: GroupsService) {
+    }
 
-  getGroups() {
-        this.groupsService.getGroups().subscribe(groups => {
+    getGroups() {
+        this.groupsService.getGroups().pipe(first()).subscribe(groups => {
             console.log('Get groups', groups)
             this.groups = groups
         })
-  }
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- ui/src/app/sidebar/sidebar.module.ts
+++ ui/src/app/sidebar/sidebar.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupsComponent } from './groups/groups.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [GroupsComponent],
+  declarations: [GroupsComponent, MenuComponent],
   exports     : [GroupsComponent]
 })
 export class SidebarModule { }
