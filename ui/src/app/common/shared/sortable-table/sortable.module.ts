4937057c15a4466191a58a6bf1efacaa75fa7317
Rostyslav Haidukov <rostyk.haidukov1999@gmail.com>
2018-07-26 13:08:27 +0300

    shared

--- ui/src/app/common/shared/sortable-table/sortable.module.ts
+++ ui/src/app/common/shared/sortable-table/sortable.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SortableColumnComponent } from './sortable-table/sortable-column/sortable-column.component';
-import { SortableTableDirective } from './sortable-table/sortable-table.directive';
-import { SortService } from '../services/sort-service.service';
+import { SortableColumnComponent } from './sortable-column/sortable-column.component';
+import { SortableTableDirective } from './sortable-table.directive';
+import { SortService } from '../../services/sort-service.service';
 
 @NgModule({
     imports     : [
